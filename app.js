fedd78d2d1ec8c75aa541b360eaae201c76595af
victoryhwan <victoryhwan7@gmail.com>
2020-05-02 22:56:04 +0900

    장보고 api 이전

---
+++ app.js
@@ -0,0 +1,72 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const HTTP = require("http");
+const HTTPS = require("https");
+var cors = require("cors");
+var path = require("path");
+var bodyParser = require("body-parser");
+// var cookieParser = require('cookie-parser');
+const formData = require("express-form-data");
+const os = require("os");
+
+app.set("port", process.env.PORT || 3031);
+
+// app.listen(port, ()=> winston.info(`Listening on port ${port}...`));
+// app.use(logger('dev'));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json({ limit: "200mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "200mb" }));
+app.use(bodyParser.text({ limit: "200mb" }));
+
+HTTP.createServer(app).listen(app.get("port"), function() {
+  //   winston.info(`Listening on port ${app.get('port')}...`)
+  console.log("Express server listening on port " + app.get("port"));
+});
+
+var allowCORS = function(req, res, next) {
+  res.header("Acess-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, Accecpt, Content-Type, Access-Control-Allow-Origin, Authorization, X-Requested-With, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+};
+
+app.use(allowCORS);
+app.use(cors());
+app.set("trust proxy", 1); // trust first proxy
+
+const options = {
+  uploadDir: os.tmpdir()
+};
+app.use(function(req, res, next) {
+  console.log(req.rawHeaders);
+  console.log(req.headers);
+  // console.log(req.rawHeaders.api_key);
+  // console.log(req);
+  // if (process.env.API_KEY != req.headers.api_key) {
+  //   res.json({
+  //     code: 410,
+  //     message: "API Key Auth Fail",
+  //     data: `${process.env.API_KEY}, ${JSON.stringify(req.headers)}`,
+  //     etc: `${JSON.stringify(req.rawHeaders)}`
+  //   });
+  // } else {
+  //   next();
+  // }
+  next();
+});
+
+app.use(formData.parse(options));
+
+app.get("/", (req, res) => res.send("Welcome to JangboGo API"));
+app.use("/member", require("./routes/api/member/controller"));
+app.use("/cart", require("./routes/api/cart/controller"));
+app.use("/auth", require("./routes/api/auth/auth"));
+app.use("/admin", require("./routes/api/admin/admin"));
---
+++ routes/api/member/mng/common.js
@@ -0,0 +1,114 @@
+var mysql_dbc = require("../../../config/db_con")();
+var connection = mysql_dbc.init();
+var express = require("express");
+var router = express.Router();
+var async = require("async");
+// const winston   = require('../../config/winston');
+var SERVER_ERR_MSG = "Server Error";
+
+router.selectMemberList = async () => {
+  return new Promise((resolve, reject) => {
+    var sql = `SELECT tm.idx
+                    , tm.id
+                    , tm.pw
+                    , DATE_FORMAT(tm.reg_date, '%Y-%c-%e %H:%i:%s') AS reg_date
+                    , DATE_FORMAT(tm.update_date, '%Y-%c-%e %H:%i:%s') AS update_date
+                 FROM tmember tm 
+                WHERE 1=1`;
+    connection.query(sql, [], (err, result) => {
+      if (err) {
+        return resolve({ code: 500, message: SERVER_ERR_MSG });
+      } else {
+        return resolve({ code: 200, message: "Success", data: result });
+      }
+    });
+  });
+};
+
+router.selectMember = async member => {
+  return new Promise(resolve => {
+    var sql = `SELECT tm.idx
+                    , tm.id
+                    , DATE_FORMAT(tm.reg_date, '%Y-%c-%e %H:%i:%s') AS reg_date
+                    , DATE_FORMAT(tm.update_date, '%Y-%c-%e %H:%i:%s') AS update_date 
+                    , DATE_FORMAT(tm.recently_login_date, '%Y-%c-%e %H:%i:%s') AS recently_login_date 
+                    FROM tmember tm 
+                WHERE 1=1 
+                    AND tm.id = ?
+                LIMIT 1`;
+    connection.query(sql, [member.id], (err, result) => {
+      if (err) {
+        return resolve({ code: 500, message: SERVER_ERR_MSG });
+      } else {
+        if (result.length > 0) {
+          //로그인성공
+          return resolve({ code: 200, message: "로그인 성공", data: result });
+        } else {
+          return resolve({ code: 201, message: "로그인 실패", data: result });
+        }
+      }
+    });
+  });
+};
+
+router.loginMember = async member => {
+  return new Promise((resolve, reject) => {
+    var sql = `UPDATE tmember SET
+               recently_login_date = NOW()
+                WHERE id = ?`;
+    connection.query(sql, [member.id], (err, result) => {
+      if (err) {
+        return reject({ code: 500, message: SERVER_ERR_MSG });
+      } else {
+        if (result.affectedRows == 0) {
+          return resolve({ code: 201, message: "Login Fail, ID can not find" });
+        } else {
+          return resolve({ code: 200, message: "Login Success" });
+        }
+      }
+    });
+  });
+};
+
+router.insertMember = async member => {
+  return new Promise((resolve, reject) => {
+    var sql = `INSERT INTO tmember (id, reg_date, update_date) 
+                                  VALUES (?, NOW(), NOW())`;
+    connection.query(sql, [member.id], (err, result) => {
+      if (err) {
+        return reject({
+          code: 500,
+          message: `${SERVER_ERR_MSG}, err : ${err}`
+        });
+      } else {
+        if (result.affectedRows == 0) {
+          return resolve({ code: 201, message: "Regist Fail" });
+        } else {
+          return resolve({ code: 200, message: "Regist Success" });
+        }
+      }
+    });
+  });
+};
+
+router.deleteMember = async member => {
+  return new Promise((resolve, reject) => {
+    var sql = `DELETE FROM tmember WHERE id = ?`;
+    connection.query(sql, [member.id], (err, result) => {
+      if (err) {
+        return reject({
+          code: 500,
+          message: `${SERVER_ERR_MSG}, err : ${err}`
+        });
+      } else {
+        if (result.affectedRows == 0) {
+          return resolve({ code: 201, message: "Delete Fail" });
+        } else {
+          return resolve({ code: 200, message: "Delete Success" });
+        }
+      }
+    });
+  });
+};
+
+module.exports = router;
